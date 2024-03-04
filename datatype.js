7bc61ce4fb50adaf1560649421f59ae642f84bd8
mayankrana2000 <mayank20092003@gmail.com>
2024-03-04 17:14:51 +0530

    assignment

--- datatype.js
+++ datatype.js
@@ -3,4 +3,8 @@ let a_string="Mayank"
 let b_boolean=true
 let c_null=null
 let d_undefined=undefined
-//
+//Question.5
+console.log(typeof(a_string))
+console.log(typeof(b_boolean))
+console.log(typeof(c_null))
+console.log(typeof(d_undefined))
